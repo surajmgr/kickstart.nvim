300815991cd6c81d79138566c668132c029d51d7
Suraj Pulami <surajpulami8@gmail.com>
2025-08-21 21:42:24 +0545

    fix: delete folds

--- lua/custom/plugins/foldoptions.lua
+++ lua/custom/plugins/foldoptions.lua
@@ -1,16 +1,22 @@
--- Keymap to enable Treesitter folding in the current buffer
+-- Keymap to toggle Treesitter folding in the current buffer
 vim.keymap.set('n', 'zF', function()
-  -- Set buffer-local folding options
-  vim.opt_local.foldmethod = 'expr'
-  vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
-  vim.opt_local.foldlevel = 99 -- start folds open
-  vim.opt_local.foldlevelstart = 99
+  local method = vim.opt_local.foldmethod:get()
 
-  -- Optional: customize folded text display
-  vim.opt_local.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g')
-        \ .' ... ' . trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
-
-  print 'Treesitter folding enabled for this buffer'
-end, { desc = 'Enable Treesitter folding' })
+  if method == 'expr' then
+    -- Switch to manual so folds can be deleted
+    vim.opt_local.foldmethod = 'manual'
+    vim.opt_local.foldexpr = ''
+    print 'Switched to manual folding (can delete folds now)'
+  else
+    -- Switch to Treesitter expression folding
+    vim.opt_local.foldmethod = 'expr'
+    vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.opt_local.foldlevel = 99
+    vim.opt_local.foldlevelstart = 99
+    vim.opt_local.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g')
+          \ .' ... ' . trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
+    print 'Treesitter folding enabled for this buffer'
+  end
+end, { desc = 'Toggle Treesitter/manual folding' })
 
 return {}
